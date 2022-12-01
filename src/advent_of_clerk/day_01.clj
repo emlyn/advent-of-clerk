8a42e151bb6374fad0b29662b3693d19e81e4c07
Emlyn Corrin <emlyn.corrin@gmail.com>
2022-12-01 13:13:50 +0000

    Day 1

--- src/advent_of_clerk/day_01.clj
+++ src/advent_of_clerk/day_01.clj
@@ -1,3 +1,27 @@
 ;; # 🎄 Advent of Clerk: Day 1
 (ns advent-of-clerk.day-01
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [nextjournal.clerk :as clerk]
+            [clojure.string :as str]))
+
+(defn parse-int [s]
+  (Integer/parseInt s))
+
+;; Part 1
+(as-> (slurp "input/day_01.txt")
+      $
+  (str/split $ #"\n\n")
+  (map #(str/split % #"\n") $)
+  (map #(map parse-int %) $)
+  (map (partial apply +) $)
+  (apply max $))
+
+;; Part 2
+(as-> (slurp "input/day_01.txt")
+      $
+  (str/split $ #"\n\n")
+  (map #(str/split % #"\n") $)
+  (map #(map parse-int %) $)
+  (map (partial apply +) $)
+  (sort $)
+  (take-last 3 $)
+  (apply + $))
