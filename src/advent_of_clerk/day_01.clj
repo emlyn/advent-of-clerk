eb512500474293de3b95e971fcc290a40e59ab78
Emlyn Corrin <emlyn.corrin@gmail.com>
2022-12-01 15:37:52 +0000

    Clean up

--- src/advent_of_clerk/day_01.clj
+++ src/advent_of_clerk/day_01.clj
@@ -1,27 +1,22 @@
-;; # 🎄 Advent of Clerk: Day 1
+;; # 🎄 Advent of Code: Day 1
 (ns advent-of-clerk.day-01
   (:require [nextjournal.clerk :as clerk]
             [clojure.string :as str]))
 
-(defn parse-int [s]
-  (Integer/parseInt s))
+;; ## Common:
+(def data
+  (->> (str/split (slurp "input/day_01.txt") #"\n\n")
+       (map #(str/split % #"\n"))
+       (map #(mapv parse-long %))))
 
-;; Part 1
-(as-> (slurp "input/day_01.txt")
-      $
-  (str/split $ #"\n\n")
-  (map #(str/split % #"\n") $)
-  (map #(map parse-int %) $)
-  (map (partial apply +) $)
-  (apply max $))
+;; ## Part 1:
+(->> data
+     (map (partial apply +))
+     (apply max))
 
-;; Part 2
-(as-> (slurp "input/day_01.txt")
-      $
-  (str/split $ #"\n\n")
-  (map #(str/split % #"\n") $)
-  (map #(map parse-int %) $)
-  (map (partial apply +) $)
-  (sort $)
-  (take-last 3 $)
-  (apply + $))
+;; ## Part 2:
+(->> data
+     (map (partial apply +))
+     (sort)
+     (take-last 3)
+     (apply +))
