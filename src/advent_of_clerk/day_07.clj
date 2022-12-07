6ae7b1d4fc1c664db4ea82ee56d9f5b84658f060
Emlyn Corrin <emlyn.corrin@gmail.com>
2022-12-07 11:59:11 +0000

    Day 7

--- src/advent_of_clerk/day_07.clj
+++ src/advent_of_clerk/day_07.clj
@@ -5,20 +5,113 @@
 
 ;; ## Common:
 
-;; DO_NOT_PUBLISH (remove this line when you edit this file)
+(defn cd [pwd dir]
+  (case dir
+    "/"  []
+    ".." (vec (butlast pwd))
+    (conj pwd dir)))
 
 (defn process [s]
-  (->> s
-       str/split-lines))
+  (loop [pwd []
+         tree {}
+         ls? false
+         [line & rest] (str/split-lines s)]
+    (cond
+      (or (not line) (empty? line))
+      tree
 
-(def ex (process ""))
+      (and ls? (str/starts-with? line "$ "))
+      (recur pwd
+             tree
+             false
+             (cons line rest))
 
-(def data (process "" #_(slurp "input/day_07.txt")))
+      (and ls? (str/starts-with? line "dir "))
+      (let [[_ fname] (str/split line #" ")]
+        (recur pwd
+               (assoc-in tree (conj pwd fname) {})
+        ls?
+        rest))
+
+      ls?
+      (let [[size fname] (str/split line #" ")
+            size (parse-long size)]
+        (recur pwd
+               (assoc-in tree (conj pwd fname) size)
+               ls?
+               rest))
+
+      (str/starts-with? line "$ cd ")
+      (recur (cd pwd (subs line 5))
+             tree
+             false
+             rest)
+
+      (str/starts-with? line "$ ls")
+      (recur pwd tree true rest)
+
+      :else
+      (throw (Exception. (format "oops: %s" line))))))
+
+(def ex (process "$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k"))
+
+(def data (process (slurp "input/day_07.txt")))
+
+(defn treesize [tree]
+  (cond
+    (map? tree)
+    (apply + (map treesize (vals tree)))
+
+    (int? tree)
+    tree
+
+    :else
+    (throw (Exception. (format "oops: %s" tree)))))
+
+(defn dirs
+  ([tree]
+   (dirs tree []))
+  ([ tree path]
+   (let [subtree (get-in tree path)]
+     (if (map? subtree)
+       (lazy-seq (cons path
+                       (mapcat (fn [d]
+                                 (dirs tree (conj path d)))
+                               (keys subtree))))
+       []))))
 
 ;; ## Part 1:
 
 (defn part1 [d]
-  (->> d))
+  (->> d
+       dirs
+       (map #(vector (if (empty? %) "/" (last %))
+                     (treesize (get-in d %))))
+       (filter #(<= (last %) 100000))
+       (map last)
+       (apply +)))
 
 (part1 ex)
 
@@ -26,10 +119,17 @@
 
 ;; ## Part 2:
 
-(defn part2 [d]
-  (->> d))
+(defn part2 [d] 
+  (let [free (- 70000000 (treesize d))
+        required (- 30000000 free)]
+    (->> d
+         dirs
+         (map #(vector (or (last %) "/")
+                       (treesize (get-in d %))))
+         (sort-by last)
+         (drop-while #(< (last %) required))
+         (first))))
 
 (part2 ex)
 
 (part2 data)
-
