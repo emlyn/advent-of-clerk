939cb59b9ddd796b30d6e2e17a3816e822fb245d
Emlyn Corrin <emlyn.corrin@gmail.com>
2022-12-09 09:46:13 +0000

    Day 9 part 1 (wrong)

--- src/advent_of_clerk/day_09.clj
+++ src/advent_of_clerk/day_09.clj
@@ -5,24 +5,81 @@
 
 ;; ## Common:
 
-;; DO_NOT_PUBLISH (remove this line when you edit this file)
-
 (defn process [s]
   (->> s
-       str/split-lines))
+       str/split-lines
+       (mapv #(vector (first %) (parse-long (str (last %)))))))
 
-(def ex (process ""))
+(def ex (process "R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2"))
 
-(def data (process "" #_(slurp "input/day_09.txt")))
+(def data (process (slurp "input/day_09.txt")))
+
+(def dirs
+  {\U [1 0]
+   \D [-1 0]
+   \L [0 -1]
+   \R [0 1]})
 
 ;; ## Part 1:
 
+(defn move [[head [tr tc]] dir]
+  (let [[nhr nhc :as nh] (mapv + head (dirs dir))
+        dr (- nhr tr)
+        dc (- nhc tc)]
+    (cond
+      (< dr -1)
+      (cond (< dc 0) [nh [(dec tr) (dec tc)]]
+            (= dc 0) [nh [(dec tr) tc]]
+            (> dc 0) [nh [(dec tr) (inc tc)]])
+      (> dr 1)
+      (cond (< dc 0) [nh [(inc tr) (dec tc)]]
+            (= dc 0) [nh [(inc tr) tc]]
+            (> dc 0) [nh [(inc tr) (inc tc)]])
+      (< dc -1)
+      (cond (< dr 0) [nh [(dec tr) (dec tc)]]
+            (= dr 0) [nh [tr       (dec tc)]]
+            (> dr 0) [nh [(inc tr) (dec tc)]])
+      (> dc 1)
+      (cond (< dr 0) [nh [(dec tr) (inc tc)]]
+            (= dr 0) [nh [tr       (inc tc)]]
+            (> dr 0) [nh [(inc tr) (inc tc)]])
+      :else
+      [nh [tr tc]])))
+
+(def start [[0 0] [0 0]])
+
+(move start \D)
+
+(reductions move
+            start
+            [\D \D \R \R])
+
 (defn part1 [d]
-  (->> d))
+  (->> d
+       (mapcat #(repeat (last %) (first %)))
+       (reductions move start)
+       (map last)
+       (set)
+       (count)))
 
 (part1 ex)
 
 (part1 data)
+
+;; 3225: That's not the right answer; your answer is too low.
+
+;; Debug:
+
+(->> ex
+     (mapcat #(repeat (last %) (first %)))
+     (reductions move start))
 
 ;; ## Part 2:
 
