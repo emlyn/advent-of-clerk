870a8d0c2b46f2648ead097f028e2ef48defffeb
Emlyn Corrin <emlyn.corrin@gmail.com>
2022-12-12 11:50:48 +0000

    Day 12 (a bit s l o w ...)

--- src/advent_of_clerk/day_12.clj
+++ src/advent_of_clerk/day_12.clj
@@ -5,20 +5,101 @@
 
 ;; ## Common:
 
-;; DO_NOT_PUBLISH (remove this line when you edit this file)
-
 (defn process [s]
   (->> s
        str/split-lines))
 
-(def ex (process ""))
+(def ex (process "Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi"))
 
-(def data (process "" #_(slurp "input/day_12.txt")))
+(def data (process (slurp "input/day_12.txt")))
+
+(def to-height
+  (into {\S 1 \E 26}
+        (for [i (range 26)]
+          [(char (+ (int \a) i))
+           (inc i)])))
 
 ;; ## Part 1:
 
-(defn part1 [d]
-  (->> d))
+(defn neighbours [[r c] grid]
+  (cond-> []
+    (pos? r) (conj [(dec r) c])
+    (pos? c) (conj [r (dec c)])
+    (< r (dec (count grid))) (conj [(inc r) c])
+    (< c (dec (count (first grid)))) (conj [r (inc c)])))
+
+(neighbours [0 1] ex)
+
+(defn valid-move? [heights from to]
+  (>= (->> from (get-in heights) to-height)
+      (->> to (get-in heights) to-height dec)))
+
+(valid-move? ex [2 0] [2 1])
+(valid-move? ex [2 0] [3 0])
+
+(defn update-grid [[heights grid] pos]
+  (if-let [nearest
+           (->> grid
+                (neighbours pos)
+                (filter #(valid-move? heights % pos))
+                (map (partial get-in grid))
+                (remove nil?)
+                (seq))]
+    [heights (assoc-in grid pos (inc (apply min nearest)))]
+    [heights grid]))
+
+(defn start-grid [heights start]
+  (assoc-in (vec (repeat (count heights)
+                         (vec (repeat (count (first heights)) nil))))
+            start
+            0))
+
+#_(start-grid ex [0 0])
+
+
+(update-grid [ex (start-grid ex [0 0])] [0 1])
+(update-grid [ex (start-grid ex [0 0])] [0 2])
+
+(defn step [heights grid]
+  (let [result
+        (reduce update-grid
+                [heights grid]
+                (for [r (range (count grid))
+                      c (range (count (first grid)))
+                      :when (nil? (get-in grid [r c]))]
+                  [r c]))]
+    (when (not= (last result) grid)
+      result)))
+
+(step ex (start-grid ex [0 0]))
+
+(defn positions [chr grid]
+  (->> grid
+       (mapv #(str/index-of % chr))
+       (map vector
+            (range))
+       (filter second)))
+
+(defn position [chr grid]
+  (first (positions chr grid)))
+
+(position \E ex)
+
+(defn distance [heights start end]
+  (loop [grid (start-grid heights start)]
+    (if-let [n (get-in grid end)]
+      n
+      (when-let [next (last (step heights grid))]
+        (recur next)))))
+
+(defn part1 [heights]
+  (distance heights
+            (position \S heights)
+            (position \E heights)))
 
 (part1 ex)
 
@@ -26,8 +107,17 @@
 
 ;; ## Part 2:
 
-(defn part2 [d]
-  (->> d))
+(positions \a ex)
+
+(defn part2 [heights]
+  (let [starts
+        (into [(position \S heights)]
+              (positions \a heights))
+        end (position \E heights)]
+    (->> starts
+         (map #(distance heights % end))
+         (remove nil?)
+         (apply min))))
 
 (part2 ex)
 
