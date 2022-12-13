b182fe69b002b22d0441ea5158b53a1e9cb2e44e
Emlyn Corrin <emlyn.corrin@gmail.com>
2022-12-13 10:44:40 +0000

    Day 13

--- src/advent_of_clerk/day_13.clj
+++ src/advent_of_clerk/day_13.clj
@@ -5,20 +5,88 @@
 
 ;; ## Common:
 
-;; DO_NOT_PUBLISH (remove this line when you edit this file)
+#_#_#_#_#_(declare parse-term)
+(defn parse-list
+  [[c & more]])
+(defn patse-int
+  [[c & more]])
+(def digit (into #{} "0123456789"))
+(defn parse-term
+  [[c & more :as s]]
+  (cond
+    (= c \[)
+    (parse-list s)
+
+    (digit c)
+    (parse-int s)))
+
+(defn parse-input
+  [s]
+  (read-string s))
 
 (defn process [s]
-  (->> s
-       str/split-lines))
+  (->> (str/split s #"\n\n")
+       (map str/split-lines)
+       (map (partial map parse-input))))
 
-(def ex (process ""))
+(def ex (process "[1,1,3,1,1]
+[1,1,5,1,1]
 
-(def data (process "" #_(slurp "input/day_13.txt")))
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]"))
+
+(def data (process (slurp "input/day_13.txt")))
 
 ;; ## Part 1:
 
+(defn compare-packets
+  [a b]
+  (cond
+    (and (int? a) (int? b))
+    (when (not= a b) (- a b))
+
+    (and (int? a) (sequential? b))
+    (recur [a] b)
+
+    (and (sequential? a) (int? b))
+    (recur a [b])
+
+    (and (sequential? a) (sequential? b))
+    (or (->> (map compare-packets a b)
+             (remove nil?)
+             first)
+        (compare-packets (count a) (count b)))))
+
+[(compare-packets 1 2)
+ (compare-packets [1] [2])
+ (compare-packets [1 1] [1 2])
+ (compare-packets [1] 2)
+ (compare-packets [2] [1 0])]
+
 (defn part1 [d]
-  (->> d))
+  (->> d
+       (map-indexed #(vector (inc %1) (apply compare-packets %2)))
+       (filter #(neg? (second %)))
+       (map first)
+       (apply +)))
 
 (part1 ex)
 
@@ -27,7 +95,13 @@
 ;; ## Part 2:
 
 (defn part2 [d]
-  (->> d))
+  (->> d
+       (mapcat identity)
+       (into [[[2]] [[6]]])
+       (sort compare-packets)
+       (map-indexed #(when (#{[[2]] [[6]]} %2) (inc %1)))
+       (remove nil?)
+       (apply *)))
 
 (part2 ex)
 
