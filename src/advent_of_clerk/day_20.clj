91009fc55f24bf91e7dc1dc6fba590f44c14cf57
Emlyn Corrin <emlyn.corrin@gmail.com>
2022-12-21 18:28:02 +0000

    Day 20

--- src/advent_of_clerk/day_20.clj
+++ src/advent_of_clerk/day_20.clj
@@ -5,20 +5,61 @@
 
 ;; ## Common:
 
-;; DO_NOT_PUBLISH (remove this line when you edit this file)
-
 (defn process [s]
   (->> s
-       str/split-lines))
+       str/split-lines
+       (mapv parse-long)))
 
-(def ex (process ""))
+(def ex (process "1
+2
+-3
+3
+-2
+0
+4"))
 
-(def data (process "" #_(slurp "input/day_20.txt")))
+(def data (process (slurp "input/day_20.txt")))
+
+(defn indexed
+  [data]
+  (vec (map-indexed vector data)))
+
+(indexed ex)
+
+(defn mix-nth
+  [idata n]
+  (let [[pos item] (->> idata
+                        (map-indexed (fn [pos [i v]]
+                                       [pos [i v] (= i n)]))
+                        (drop-while (comp false? last))
+                        (first))
+        temp-data (into (subvec idata 0 pos) (subvec idata (inc pos)))
+        new-pos (inc (mod (dec (+ pos (second item))) (count temp-data)))]
+    (into (conj (subvec temp-data 0 new-pos) item)
+          (subvec temp-data new-pos))))
+
+(mix-nth (indexed ex) 2)
+
+(defn mix-round
+  [d]
+  (reduce mix-nth
+          d
+          (range (count d))))
+
+(->> ex indexed mix-round (mapv last))
 
 ;; ## Part 1:
 
 (defn part1 [d]
-  (->> d))
+  (let [mixed (->> d indexed mix-round (mapv last))
+        zpos
+        (->> mixed
+             (map-indexed (fn [_ v] (zero? v)))
+             (take-while false?)
+             (count))]
+    (apply +
+           (map #(get mixed (mod (+ zpos %) (count d)))
+                [1000 2000 3000]))))
 
 (part1 ex)
 
@@ -26,8 +67,29 @@
 
 ;; ## Part 2:
 
+(->> ex
+     (map (partial * 811589153))
+     (indexed)
+     (iterate mix-round)
+     (drop 10)
+     (first)
+     (mapv last))
+
 (defn part2 [d]
-  (->> d))
+  (let [mixed (->> d
+                   (map (partial * 811589153))
+                   (indexed)
+                   (iterate mix-round)
+                   (drop 10)
+                   (first)
+                   (mapv last))
+        zpos (->> mixed
+                  (map-indexed (fn [_ v] (zero? v)))
+                  (take-while false?)
+                  (count))]
+    (apply +
+           (map #(get mixed (mod (+ zpos %) (count d)))
+                [1000 2000 3000]))))
 
 (part2 ex)
 
