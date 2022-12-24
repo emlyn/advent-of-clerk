b6c5f669fff36bd8a2bdc4e493406cdd494d5e01
Emlyn Corrin <emlyn.corrin@gmail.com>
2022-12-24 01:01:40 +0000

    Day 18 (part 1, part 2 not correct)

--- src/advent_of_clerk/day_18.clj
+++ src/advent_of_clerk/day_18.clj
@@ -5,20 +5,50 @@
 
 ;; ## Common:
 
-;; DO_NOT_PUBLISH (remove this line when you edit this file)
-
 (defn process [s]
   (->> s
-       str/split-lines))
+       str/split-lines
+       (map #(str/split % #","))
+       (map (partial mapv parse-long))
+       (set)))
 
-(def ex (process ""))
+(def ex (process "2,2,2
+1,2,2
+3,2,2
+2,1,2
+2,3,2
+2,2,1
+2,2,3
+2,2,4
+2,2,6
+1,2,5
+3,2,5
+2,1,5
+2,3,5"))
 
-(def data (process "" #_(slurp "input/day_18.txt")))
+(def data (process (slurp "input/day_18.txt")))
 
 ;; ## Part 1:
 
+(defn neighbours
+  [[x y z]]
+  [[(inc x) y z]
+   [(dec x) y z]
+   [x (inc y) z]
+   [x (dec y) z]
+   [x y (inc z)]
+   [x y (dec z)]])
+
+(neighbours [1 2 3])
+
 (defn part1 [d]
-  (->> d))
+  (let [[seen ntouch]
+        (reduce (fn [[seen ntouch] block]
+                  [(conj seen block) (+ ntouch (count (filter seen (neighbours block))))])
+                [#{} 0]
+                d)]
+    (- (* (count seen) 6)
+       (* ntouch 2))))
 
 (part1 ex)
 
@@ -26,8 +56,34 @@
 
 ;; ## Part 2:
 
+(defn extent
+  [d]
+  (map #(->> d
+             (map %)
+             (apply (juxt min max)))
+       [first second last]))
+
+(extent ex)
+
 (defn part2 [d]
-  (->> d))
+  (let [[[x1 x2] [y1 y2] [z1 z2]] (extent d)
+        outer (into {}
+                    (for [x (range x1 (inc x2))
+                          y (range y1 (inc y2))
+                          z (range z1 (inc z2))
+                          :let [p [x y z]]
+                          :when (not (d p))]
+                      [[x y z] (boolean (or (#{x1 x2} x)
+                                            (#{y1 y2} y)
+                                            (#{z1 z2} z)))]))]
+    (loop [outer outer]
+      (let [next (reduce-kv (fn [m k v]
+                              (assoc m k (or v (some outer (neighbours k)))))
+                            {}
+                            outer)]
+        (if (= next outer)
+          (->> next vals (remove identity) (count) (- (part1 d)))
+          (recur next))))))
 
 (part2 ex)
 
